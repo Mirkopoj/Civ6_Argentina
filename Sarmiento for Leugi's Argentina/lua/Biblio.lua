98e31d666f41800811ac6b09ec853b4ccaecdbb7
MirkoPoj <mirkopoj@hotmail.com>
2023-02-28 11:32:16 -0800

    City

--- Sarmiento for Leugi's Argentina/lua/Biblio.lua
+++ Sarmiento for Leugi's Argentina/lua/Biblio.lua
@@ -12,7 +12,7 @@ function add_science_per_trade(PlayerID, OriginPlayerID, OriginCityID, TargetPla
 
 	print("Llego a sarmiento")
 
-	local pCity = Cities[TargetCityID]
+	local pCity = City[TargetCityID]
 
 	if pCity == nil then
 		print ("pCity nil")
