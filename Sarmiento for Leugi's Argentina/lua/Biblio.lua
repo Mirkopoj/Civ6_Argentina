9ac5bf2df7d6985986216c953b008b1fbe4f8f3c
MirkoPoj <mirkopoj@hotmail.com>
2023-02-28 10:45:48 -0800

    Con Civics

--- Sarmiento for Leugi's Argentina/lua/Biblio.lua
+++ Sarmiento for Leugi's Argentina/lua/Biblio.lua
@@ -4,6 +4,42 @@
 --------------------------------------------------------------
 local iBiblio = GameInfo.Buildings["BUILDING_MRK_BIB_POPULAR"].Index
 
+function get_random_tech_boost(pSarmiento)
+	local pTech = pSarmiento:GetTechs()
+	for Tech in GameInfo.Technologies() do
+		local iTech = Tech.Index
+		print(iTech)
+		print(Tech.TechnologyType)
+		local bStatus = pTech:HasBoostBeenTriggered(iTech)
+		local hasTech = pTech:HasTech(iTech)
+		print(bStatus)
+		print(hasTech)
+		if not bStatus and not hasTech then
+			pTech:TriggerBoost(iTech)
+			print("Boosted")
+			break;
+		end
+	end
+end
+
+function get_random_principle_boost(pSarmiento)
+	local pPrinc = pSarmiento:GetCulture()
+	for Princ in GameInfo.Civics() do
+		local iPrinc = Princ.Index
+		print(iPrinc)
+		print(Princ.CivicType)
+		local bStatus = pPrinc:HasBoostBeenTriggered(iPrinc)
+		local hasPrinc = pPrinc:HasCivic(iPrinc)
+		print(bStatus)
+		print(hasPrinc)
+		if not bStatus and not hasPrinc then
+			pPrinc:TriggerBoost(iPrinc)
+			print("Boosted")
+			break;
+		end
+	end
+end
+
 function add_science_per_trade(PlayerID, OriginPlayerID, OriginCityID, TargetPlayerID, TargetCityID)
 
 	if PlayerConfigurations[TargetPlayerID]:GetLeaderTypeName() ~= "LEADER_MRK_SARMIENTO" then
@@ -25,21 +61,8 @@ function add_science_per_trade(PlayerID, OriginPlayerID, OriginCityID, TargetPla
 
 	print("Tenia biblio")
 
-	local pTech = pSarmiento:GetTechs()
-	for Tech in GameInfo.Technologies() do
-		local iTech = Tech.Index
-		print(iTech)
-		print(Tech.TechnologyType)
-		local bStatus = pTech:HasBoostBeenTriggered(iTech)
-		local hasTech = pTech:HasTech(iTech)
-		print(bStatus)
-		print(hasTech)
-		if not bStatus and not hasTech then
-			pTech:TriggerBoost(iTech)
-			print("Boosted")
-			break;
-		end
-	end
+	get_random_tech_boost(pSarmiento)
+	get_random_principle_boost(pSarmiento)
 	
 end
 
