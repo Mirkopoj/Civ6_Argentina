488f9ea5e048314ef9588c5efc77eb0bc2a1f87e
MirkoPoj <mirkopoj@hotmail.com>
2023-03-01 12:14:21 -0800

    UnitCommandStarted

--- Sarmiento for Leugi's Argentina/lua/SarmientoCombat.lua
+++ Sarmiento for Leugi's Argentina/lua/SarmientoCombat.lua
@@ -3,12 +3,12 @@
 -- DateCreated: 3/1/2023 3:20:32 PM
 --------------------------------------------------------------
 
-function sarmiento_combat_strength()
-	print("Activada")
+function sarmiento_combat_strength(playerID, unitID, hCommand, iData1)
+	print(hCommand..iData1)
 end
 
 function MRKSarmientoCombatInit()
-	Events.UnitActivate.Add(sarmiento_combat_strength)
+	Events.UnitCommandStarted.Add(sarmiento_combat_strength)
 end
 
 Events.LoadScreenClose.Add(MRKSarmientoCombatInit)
