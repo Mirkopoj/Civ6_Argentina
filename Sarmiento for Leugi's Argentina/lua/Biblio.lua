7dfaeac7e9507bae449dc38d91c1a89283b4b46f
MirkoPoj <mirkopoj@hotmail.com>
2023-03-01 10:44:40 -0800

    Sintax error sin then en elseif

--- Sarmiento for Leugi's Argentina/lua/Biblio.lua
+++ Sarmiento for Leugi's Argentina/lua/Biblio.lua
@@ -76,7 +76,7 @@ function add_science_per_trade(PlayerID, OriginPlayerID, OriginCityID, TargetPla
 	local rand = math.random(5)
 	if rand == 1 then
 		get_random_tech_boost(pSarmiento)
-	else if rand == 2 then
+	elseif rand == 2 
 		get_random_principle_boost(pSarmiento)
 	else
 		add_citizen(pCity)
