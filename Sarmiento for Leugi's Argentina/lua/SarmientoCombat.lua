df4a46465629de8fd95fb63dbb4a75d4295106b5
Mirkopoj <mirkopoj@hotmail.com>
2023-03-02 12:26:01 -0800

    PLayerManager index

--- Sarmiento for Leugi's Argentina/lua/SarmientoCombat.lua
+++ Sarmiento for Leugi's Argentina/lua/SarmientoCombat.lua
@@ -26,7 +26,7 @@ end
 
 function sarmiento_combat_update(iSarmiento)
 	local max_bonus = 0
-	for iPlayer = 0, GameConfiguration[1]:GetAvailablePlayerCount()-1, 1 do
+	for iPlayer = 0, PlayerManager[0]:GetWasEverAliveMajorCount()-1, 1 do
 		local pPlayer = Players[iPlayer]
 		print(iPlayer)
 		print(pPlayer)
@@ -47,7 +47,7 @@ function sarmiento_combat_update(iSarmiento)
 end
 
 function sarmiento_combat_strength_turn()
-	for iPlayer = 0, PlayerManager:GetWasEverAliveMajorCount()-1, 1 do
+	for iPlayer = 0, PlayerManager[0]:GetWasEverAliveMajorCount()-1, 1 do
 		if PlayerConfigurations[iPlayer]:GetLeaderTypeName() == "LEADER_MRK_SARMIENTO" then
 			sarmiento_combat_update(iPlayer)
 		end
